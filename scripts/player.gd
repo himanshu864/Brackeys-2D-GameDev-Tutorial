9a421a2b662350b46023ca615dfa6fb28e34cf3a
himanshu864 <himanshu864000@gmail.com>
2025-02-17 16:33:35 +0530

    1.0 worldbuilding; success; on to game jam

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,15 +4,23 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
+var doubleJumpRemaining = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		doubleJumpRemaining = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		else:
+			if doubleJumpRemaining:
+				doubleJumpRemaining = false
+				velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
