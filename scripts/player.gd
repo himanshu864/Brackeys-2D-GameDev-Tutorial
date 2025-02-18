47d30281b9d6c23157c0d815c9c37ed4d7b4290a
himanshu864 <himanshu864000@gmail.com>
2025-02-18 20:41:32 +0530

    brackeys tutorial completed + deployed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
@@ -13,7 +15,7 @@ func _physics_process(delta: float) -> void:
 		doubleJumpRemaining = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		else:
@@ -21,9 +23,27 @@ func _physics_process(delta: float) -> void:
 				doubleJumpRemaining = false
 				velocity.y = JUMP_VELOCITY
 
+	var direction := Input.get_axis("move_left", "move_right")
+
+	# Flip sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true	
+
+	# Play animations
+	if not is_on_floor():
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+	else:
+		if velocity.x == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
